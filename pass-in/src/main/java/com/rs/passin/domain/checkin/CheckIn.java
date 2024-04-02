4c26b75236d4c7aa362ed29ab2e6a06b2a46537b
Bruno H. Homem <brunohhomem@gmail.com>
2024-04-02 20:20:52 -0300

    added entities

---
+++ pass-in/src/main/java/com/rs/passin/domain/checkin/CheckIn.java
@@ -0,0 +1,31 @@
+package com.rs.passin.domain.checkin;
+
+import com.rs.passin.domain.attendee.Attendee;
+import com.rs.passin.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "check_ins")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckIn {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "attendee_id", nullable = false)
+    private Attendee attendee;
+}

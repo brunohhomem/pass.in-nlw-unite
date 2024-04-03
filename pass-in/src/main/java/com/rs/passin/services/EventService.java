5d981a4cbf3fd13fb5da779d3b27b2b53a2c3fcb
Bruno H. Homem <brunohhomem@gmail.com>
2024-04-03 19:17:32 -0300

    added: service

---
+++ pass-in/src/main/java/com/rs/passin/services/EventService.java
@@ -0,0 +1,52 @@
+package com.rs.passin.services;
+
+import com.rs.passin.domain.attendee.Attendee;
+import com.rs.passin.domain.event.Event;
+import com.rs.passin.dto.event.EventIdDto;
+import com.rs.passin.dto.event.EventRequestDTO;
+import com.rs.passin.dto.event.EventResponseDTO;
+import com.rs.passin.repositories.AttendeeRepository;
+import com.rs.passin.repositories.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.text.Normalizer;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+
+    private final EventRepository eventRepository;
+    private final AttendeeRepository attendeeRepository;
+
+    public EventResponseDTO getEventDetail(String eventId){
+        Event event = this.eventRepository.findById(eventId)
+                .orElseThrow(() -> new RuntimeException("Event not found with ID: " + eventId));
+
+        List<Attendee> attendeeList = this.attendeeRepository.findByEventId(eventId);
+
+        return new EventResponseDTO(event, attendeeList.size());
+    }
+
+    public EventIdDto createEvent(EventRequestDTO dto){
+        Event newEvent = new Event();
+
+        newEvent.setTitle(dto.title());
+        newEvent.setDetails(dto.details());
+        newEvent.setMaximumAttendees(dto.maximumAttendees());
+        newEvent.setSlug(createSlug(dto.title()));
+
+        this.eventRepository.save(newEvent);
+
+        return new EventIdDto(newEvent.getId());
+    }
+
+    private String createSlug(String text){
+        String normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
+        return normalized.replaceAll("\\p{InCOMBINING_DIACRITICAL_MARKS}", "")
+                .replaceAll("[^\\w\\s]", "")
+                .replaceAll("\\s+", "-")
+                .toLowerCase();
+    }
+}

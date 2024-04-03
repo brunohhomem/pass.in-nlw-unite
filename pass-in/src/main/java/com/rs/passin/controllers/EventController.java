291376f4359d39467672a0bf8cae617d7a4c4f19
Bruno H. Homem <brunohhomem@gmail.com>
2024-04-03 19:17:44 -0300

    added: get and post events

--- pass-in/src/main/java/com/rs/passin/controllers/EventController.java
+++ pass-in/src/main/java/com/rs/passin/controllers/EventController.java
@@ -1,16 +1,34 @@
 package com.rs.passin.controllers;
 
+import com.rs.passin.dto.event.EventIdDto;
+import com.rs.passin.dto.event.EventRequestDTO;
+import com.rs.passin.dto.event.EventResponseDTO;
+import com.rs.passin.services.EventService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
 
-    @GetMapping
-    public ResponseEntity<String> getTeste(){
-        return ResponseEntity.ok("sucess");
+    private final EventService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id){
+        EventResponseDTO event = service.getEventDetail(id);
+
+        return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDto> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        EventIdDto eventIdDto = service.createEvent(body);
+
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDto.id()).toUri();
+
+        return ResponseEntity.created(uri).body(eventIdDto);
     }
 }
--- pass-in/src/main/java/com/rs/passin/repositories/AttendeeRepository.java
+++ pass-in/src/main/java/com/rs/passin/repositories/AttendeeRepository.java
@@ -3,5 +3,9 @@ package com.rs.passin.repositories;
 import com.rs.passin.domain.attendee.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+
+    List<Attendee> findByEventId(String eventId);
 }

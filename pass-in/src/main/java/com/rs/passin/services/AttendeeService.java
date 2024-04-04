5f6596b6e300c7a30db13ea7b28254885b33ca8d
Bruno H. Homem <brunohhomem@gmail.com>
2024-04-04 06:59:42 -0300

    added Attendee badges

--- pass-in/src/main/java/com/rs/passin/services/AttendeeService.java
+++ pass-in/src/main/java/com/rs/passin/services/AttendeeService.java
@@ -2,13 +2,17 @@ package com.rs.passin.services;
 
 import com.rs.passin.domain.attendee.Attendee;
 import com.rs.passin.domain.attendee.exceptions.AttendeeAlreadyExistException;
+import com.rs.passin.domain.attendee.exceptions.AttendeeNotFoundException;
 import com.rs.passin.domain.checkin.CheckIn;
+import com.rs.passin.dto.attendee.AttendeeBadgeResponseDTO;
 import com.rs.passin.dto.attendee.AttendeeDetails;
 import com.rs.passin.dto.attendee.AttendeesListResponseDTO;
+import com.rs.passin.dto.attendee.AttendeeBadgeDTO;
 import com.rs.passin.repositories.AttendeeRepository;
 import com.rs.passin.repositories.CheckinRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -43,10 +47,27 @@ public class AttendeeService {
     public void verifyAttendeeSubscription(String eventId, String email){
         Optional<Attendee> isAttendeeRegistered = attendeeRepository.findByEventIdAndEmail(eventId, email);
 
-        if (isAttendeeRegistered.isPresent()) throw new AttendeeAlreadyExistException("Attendee is already registered.");
+        if (isAttendeeRegistered.isPresent())
+            throw new AttendeeAlreadyExistException("Attendee is already registered.");
     }
 
     public Attendee registerAttendee(Attendee newAttendee){
         return attendeeRepository.save(newAttendee);
+    }
+
+    public AttendeeBadgeResponseDTO getAttendeeBadge(String attendeeId, UriComponentsBuilder uriComponentsBuilder){
+        Attendee attendee = attendeeRepository.findById(attendeeId)
+                .orElseThrow(() -> new AttendeeNotFoundException("Attendee not found with Id: " + attendeeId));
+
+        var uri = uriComponentsBuilder.path("/attendees/{attendeeId}/check-in")
+                .buildAndExpand(attendeeId).toUri().toString();
+
+        AttendeeBadgeDTO newAttendeeBadgeDTO = new AttendeeBadgeDTO(
+                attendee.getName(),
+                attendee.getEmail(),
+                uri,
+                attendee.getEvent().getId()
+        );
+        return new AttendeeBadgeResponseDTO(newAttendeeBadgeDTO);
     }
 }
